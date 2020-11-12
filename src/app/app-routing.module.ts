5e23afdb1cc6e703fe60f79147f9b1588d6a1524
José Mejias <jose_mejias92@hotmail.com>
2020-11-12 20:02:32 +0100

    axios en service y routing de codigos postales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PostCodesComponent} from './pages/post-codes/post-codes.component';
+import {ProductsComponent} from './pages/products/products.component';
+import {ShopsComponent} from './pages/shops/shops.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PostCodesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
